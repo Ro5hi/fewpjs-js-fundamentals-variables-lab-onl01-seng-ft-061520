1493ae71cde7388da5ada221e1762c62d1cba8ac
Shirlen Detablan <slen.detablan@gmail.com>
2020-09-15 20:20:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file!
 let companyName = 'Scuber';
 let companyCeo = 'Susan Smith';
-let mostProfitableNeighborhood = 'Chelsea';
+let mostProfitableNeighborhood = 'Chelsea'
